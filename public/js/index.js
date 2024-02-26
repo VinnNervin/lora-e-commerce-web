933bdb6df609ba64a09f6148268b58c4715d7852
Vin Nervin <charlesdbz.160@gmail.com>
2024-02-26 17:43:51 +0700

    update v.1

--- public/js/index.js
+++
@@ -1,7 +0,0 @@
-const navLinks = document.querySelector('.nav-links');
-const hamburger = document.querySelector('.bi-list')
-
-// hamburger.addEventListener('click', () => {
-//     alert('isjisnisjh')
-//     navLinks.style.translate = '200px';
-// })
